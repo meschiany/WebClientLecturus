1780bbb916ca1c9bcc75f6cf8e1804fdd251db71
EC2 Default User <ec2-user@ip-172-31-10-196.us-west-2.compute.internal>
2016-07-03 23:05:43 +0000

    show many

--- app/js/content.js
+++ app/js/content.js
@@ -24,26 +24,27 @@ function setFile(res){
 	var fileType = res.content.substr(res.content.lastIndexOf('.')).toLowerCase();
 	if (fileType == ".png" || fileType == ".jpg" || fileType == ".jpeg" || fileType == ".gif"){
 		//TODO change the file path of the production
-		$("#preview").html('<img style="position: relative;width: 100%;" src="'+consts.SERVER+'/uploads/vits/'+res.video_id+'/files/'+res.content+'">');
+		$("#contents").append('<img style="position: relative;width: 30%;" src="'+res.content+'">');
 	}else{
-		$("#preview").html('<a target="_blank" style="width:200px;height:200px;" href="'+consts.SERVER+'/uploads/vits/'+res.video_id+'/files/'+res.content+'">View/Download file:'+res.content+' </a>');
+		$("#contents").append('<a target="_blank" style="width:200px;height:200px;" href="'+res.content+'">View/Download file:'+res.content+' </a>');
 	}
 	
 }
 
 function setPreview(){
-	if (contents.length === 0){
-		return;
-	}
-	var res = getContent($('video')[0].currentTime);
-	switch (res.content_type){
-		case "text":
-			setText(res);
-			break;
-		case "file":
-			setFile(res);
-			break;
-	}
+	printData()
+	// if (contents.length === 0){
+	// 	return;
+	// }
+	// var res = getContent($('video')[0].currentTime);
+	// switch (res.content_type){
+	// 	case "text":
+	// 		setText(res);
+	// 		break;
+	// 	case "file":
+	// 		setFile(res);
+	// 		break;
+	// }
 }
 
 function printData(){
@@ -55,15 +56,20 @@ function printData(){
 	if (contents[0].content_type == "text"){
 		$("#contents").append(contents[0].content);		
 	}else{
-		$("#contents").append("FILE NAME: "+ contents[0].content);	
+		setFile(contents[i]);
+		// $("#contents").append("FILE NAME: "+ contents[0].content);	
 	}
-	
+	sec = $('video')[0].currentTime;
 	for (var i = 1; i < contents.length; i++) {
-		$("#contents").append("<div class=lineSep></div>");
+		if (contents[i].second >= sec && contents[i].second <= sec+5){
+			$("#contents").append("<div class=lineSep></div>");
 		if (contents[i].content_type == "text"){
 			$("#contents").append(contents[i].content);		
 		}else{
-			$("#contents").append("FILE NAME: "+contents[i].content);	
+			setFile(contents[i]);
+			// $("#contents").append("FILE NAME: "+contents[i].content);	
+		}	
 		}
+		
 	}
 }
